232f1382629204b33a09d306b9fffbb8ef36b11a
mh-anwar <mohammad.anwar2939@gmail.com>
2024-04-08 02:25:15 -0400

    Add file sorting logic (moving)

---
+++ projectSorter.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+echo "Where are all the projects located?  Current Dir: $(pwd)"
+printf "%s/" "$HOME"
+read sourcePath
+
+echo "Where would you like to move the projects to?"
+printf "%s/" "$HOME"
+read destinationPath
+
+# Check if source path exists
+if [ ! -d "$HOME/$sourcePath" ]; then
+    echo "\nSource path does not exist."
+    exit 1
+fi
+
+# Check if destination path exists
+if [ ! -d "$HOME/$destinationPath" ]; then
+    echo "\nDestination path does not exist."
+    exit 1
+fi
+
+# Check if .projekts file exists
+if [ ! -f ".projekts" ]; then
+    echo "\n.projekts file not found."
+    exit 1
+fi
+
+# Read .projekts file, line by line
+while IFS= read -r line
+do 
+    # Move each file from source to destination
+    # Copy the file for now (later it will be mv)
+    cp "$HOME/$sourcePath/$line" "$HOME/$destinationPath"
+done < ".projekts"
+
+sourcePath="/home/mohammad/Downloads"
+
+
+
