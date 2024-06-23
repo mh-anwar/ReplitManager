7295e73e17a3f7b9a74211fdeaef7c6404ef1ff2
mh-anwar <mohammad.anwar2939@gmail.com>
2024-06-23 14:07:35 -0400

    Improve error handling, add options

--- projectDownloader.js
+++ projectDownloader.js
@@ -1,5 +1,6 @@
 /*
 	Date: 2024-04-07
+	Updated: 2024-06-23
 	This program is designed to download all projects from a Replit team. It heavily depends on REPLIT UI not changing.
 	To run this program, you must have Node.js installed on your computer and you must install selenium-webdriver using npm.
 
@@ -7,8 +8,6 @@
 */
 
 const { Builder, By, Browser, until } = require('selenium-webdriver');
-// Get user to enter their email and password
-// Ask user for input
 const readline = require('readline');
 const fs = require('fs');
 const rl = readline.createInterface({
@@ -22,12 +21,24 @@ let email = '';
 let password = '';
 let timeData = 30;
 let teamName = 'ics4u-40-buckland';
+let browser = 'CHROME';
 
 function askQuestion(question) {
 	return new Promise((resolve, reject) => {
-		rl.question(question, (answer) => {
-			resolve(answer);
-		});
+		function prompt() {
+			rl.question(question, (answer) => {
+				if (answer.trim() !== '') {
+					resolve(answer);
+				} else {
+					console.log(
+						'\x1b[31m%s\x1b[0m',
+						'Please provide a non-empty answer.'
+					);
+					prompt(); // Prompt again
+				}
+			});
+		}
+		prompt(); // Initial prompt
 	});
 }
 
@@ -65,81 +76,112 @@ async function getUserInput() {
 		);
 		process.exit(0);
 	}
+	// Check to make sure that browser is a valid input
+	browser = await askQuestion(
+		`What browser would you like to use from \x1b[34mCHROME, EDGE, FIREFOX, SAFARI \x1b[0m (only Chrome support is guaranteed): `
+	);
+	browser = browser.toUpperCase();
+	while (
+		browser.toLowerCase() !== 'chrome' &&
+		browser.toLowerCase() !== 'edge' &&
+		browser.toLowerCase() !== 'firefox' &&
+		browser.toLowerCase() !== 'safari'
+	) {
+		console.log('CHROME, EDGE, FIREFOX, SAFARI');
+		browser = await askQuestion(
+			`What browser would you like to use (only Chrome support is guaranteed): `
+		);
+		browser = browser.toUpperCase();
+	}
 	rl.close();
 }
-
+process.on('SIGINT', function () {
+	console.log('/n So you want to exit? 😄');
+	process.exit();
+});
 getUserInput().catch((error) => {
 	console.error(error); // Fine, we'll throw the error
 });
 
 // Wait for the question to be answered
 rl.on('close', () => {
+	console.log('\n\x1b[34mStarting download processes\x1b[0m');
 	console.log(
-		'Using email and password to login: ' + email + ' and ' + password
+		'Using email and password to login: ' +
+			email +
+			' and ' +
+			password +
+			' on browser ' +
+			browser
 	);
 
 	(async function () {
-		let driver = await new Builder().forBrowser(Browser.CHROME).build();
+		const driver = await new Builder().forBrowser(Browser[browser]).build();
+		try {
+			await driver.get('https://replit.com/login');
 
-		await driver.get('https://replit.com/login');
+			// Login Process (assumes the user has provided correct username and password)
+			// Enter email and password and login
+			await driver.findElement({ id: 'username-:r0:' }).sendKeys(email);
+			await driver
+				.findElement({ id: 'password-:r6:' })
+				.sendKeys(password);
+			await driver.findElement(By.css('[data-cy="log-in-btn"]')).click();
 
-		// Login Process (assumes the user has provided correct username and password)
-		// Enter email and password and login
-		await driver.findElement({ id: 'username-:r0:' }).sendKeys(email);
-		await driver.findElement({ id: 'password-:r6:' }).sendKeys(password);
-		await driver.findElement(By.css('[data-cy="log-in-btn"]')).click();
+			// Wait for login to complete
+			await driver.wait(
+				until.elementLocated(By.css('[data-cy="home-text"]')),
+				10000
+			);
 
-		// Wait for login to complete
-		await driver.wait(
-			until.elementLocated(By.css('[data-cy="home-text"]')),
-			10000
-		);
+			// Switch to teams page
+			await driver.get('https://replit.com/team/' + teamName);
 
-		// Switch to teams page
-		await driver.get('https://replit.com/team/' + teamName);
-
-		// Find all <a> elements with href containing the teamName
-		const links = await driver.findElements(
-			By.xpath(
-				'//a[contains(@href, "@' +
-					teamName +
-					'/") and contains(text(), "Continue working")]'
-			)
-		);
-		console.log('Got all the links to the projects');
-		// Extract href attribute values and store them in an array
-		hrefs = await Promise.all(
-			links.map(async (link) => {
-				return await link.getAttribute('href');
-			})
-		);
-		console.log('Getting relevant download elements');
-		if (hrefs.length === 0) {
-			// Retry if no links are found --> check if
-			// Wait for an H4 element to appear, it's text is `Unit 1 - Examples`
-			waitForProjectsToLoad(driver);
+			// Find all <a> elements with href containing the teamName
+			const links = await driver.findElements(
+				By.xpath(
+					'//a[contains(@href, "@' +
+						teamName +
+						'/") and contains(text(), "Continue working")]'
+				)
+			);
+			console.log('Got all the links to the projects');
+			// Extract href attribute values and store them in an array
 			hrefs = await Promise.all(
 				links.map(async (link) => {
 					return await link.getAttribute('href');
 				})
 			);
+			console.log('Getting relevant download elements');
+			if (hrefs.length === 0) {
+				// Retry if no links are found --> check if
+				// Wait for an H4 element to appear, it's text is `Unit 1 - Examples`
+				waitForProjectsToLoad(driver);
+				hrefs = await Promise.all(
+					links.map(async (link) => {
+						return await link.getAttribute('href');
+					})
+				);
+			}
+			// DEBUGGER: console.log('Links with href containing "@' + teamName + '/":', hrefs);
+			console.log('\nDownloading projects');
+			// Open all these URL's starting with the first one
+			for (let i = 0; i < hrefs.length; i++) {
+				// Append `.zip` to the end of each of these to initiate downloading them
+				const url = hrefs[i] + '.zip';
+				console.log(url);
+				await driver.executeScript(`window.open('${url}', '_blank');`);
+				// Wait for 1 minute before downloading next project
+				await new Promise((resolve) =>
+					setTimeout(resolve, timeData * 1000)
+				);
+			}
+		} catch (error) {
+			console.error(error);
+		} finally {
+			// Close the browser
+			await driver.quit();
 		}
-		// DEBUGGER: console.log('Links with href containing "@' + teamName + '/":', hrefs);
-		console.log('Downloading projects');
-		// Open all these URL's starting with the first one
-		for (let i = 0; i < hrefs.length; i++) {
-			// Append `.zip` to the end of each of these to initiate downloading them
-			const url = hrefs[i] + '.zip';
-			await driver.executeScript(`window.open('${url}', '_blank');`);
-			// Wait for 1 minute before downloading next project
-			await new Promise((resolve) =>
-				setTimeout(resolve, timeData * 1000)
-			);
-		}
-
-		// Close the browser
-		await driver.quit();
-
 		// Process and clean file names to put them into a .projects file
 		// Remove https://replit.com/teamname/ from the front of the string, add .zip to the end of the string
 		for (let i = 0; i < hrefs.length; i++) {
@@ -153,6 +195,9 @@ rl.on('close', () => {
 		// Write all these URL's to a file
 		fs.writeFileSync('.projekts', projectNames.join('\n'));
 	})();
+	console.log(
+		'All projects downloaded, program complete, now run `bash projectSorter.sh`'
+	);
 });
 
 async function waitForProjectsToLoad(driver) {
